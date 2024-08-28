69e7f5d0dc580df582c6f67ee5a013e45fdd9e58
L01C4 <boy-unfind@proton.me>
2024-08-28 09:16:10 +0300

    feat: filter work for the two diagram

--- src/components/Object/ObjectTable.jsx
+++ src/components/Object/ObjectTable.jsx
@@ -1,74 +1,8 @@
 import PropTypes from 'prop-types'
-import {Handle, Position} from '@xyflow/react'
-import React from 'react'
-import {
-    LifeCycleManagedAttributes,
-    TypeManagedAttributes,
-    ThumbedAttributes,
-    NumberableAttributes,
-    IteratedAttributes,
-    VersionedAttributes,
-    ContentHolderAttributes,
-    WorkableAttributes,
-    ContactableAttributes,
-} from '../knoers/KnoersAttributes'
-import {Tr} from '../Tr'
+import {Table} from '../Table'
 
 export const ObjectTable = ({data}) => {
-    if (!data?.entity.attributes) {
-        return <div>No data available</div>
-    }
-
-    return (
-        <div className="text-updater-node">
-            <Handle type="source" position={Position.Right} id="a" />
-            <Handle type="source" position={Position.Left} id="b" />
-            <table className="minimalistBlack item">
-                <thead className="custom-drag-handle">
-                    <tr>
-                        <th colSpan={2}>
-                            {data.entity.name || 'Unknown Table'}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.entity.attributes.map(attr => (
-                        <Tr
-                            key={attr.id}
-                            attributes={attr.name}
-                            type={attr.type}
-                        />
-                    ))}
-                    {Array.isArray(data.entity.knoers) &&
-                        data.entity.knoers.map((knoer, index) => (
-                            <React.Fragment key={knoer.id || knoer + index}>
-                                {knoer === 'LifecycleManaged' && (
-                                    <LifeCycleManagedAttributes />
-                                )}
-                                {knoer === 'TypeManaged' && (
-                                    <TypeManagedAttributes />
-                                )}
-                                {knoer === 'Thumbed' && <ThumbedAttributes />}
-                                {knoer === 'Numberable' && (
-                                    <NumberableAttributes />
-                                )}
-                                {knoer === 'Iterated' && <IteratedAttributes />}
-                                {knoer === 'Versioned' && (
-                                    <VersionedAttributes />
-                                )}
-                                {knoer === 'ContentHolder' && (
-                                    <ContentHolderAttributes />
-                                )}
-                                {knoer === 'Workable' && <WorkableAttributes />}
-                                {knoer === 'Contactable' && (
-                                    <ContactableAttributes />
-                                )}
-                            </React.Fragment>
-                        ))}
-                </tbody>
-            </table>
-        </div>
-    )
+    return <Table data={data} />
 }
 
 ObjectTable.propTypes = {
